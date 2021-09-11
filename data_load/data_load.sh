58615c2650364ee210997d67e2249fba1c07403f
Bryan Freeman <bryanfree66@gmail.com>
2021-09-11 15:58:07 -0500

    fixed mongo data type problems.

--- data_load/data_load.sh
+++ data_load/data_load.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-mongoimport --host 127.0.0.1:27017 -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin -d titanic -c passengers --type csv --file /data_load/train.csv --headerline --drop
+mongoimport --host 127.0.0.1:27017  -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" \
+      --authenticationDatabase admin -d titanic -c passengers \
+      --type csv --columnsHaveTypes --fields "PassengerId.int32(),Survived.boolean(),Pclass.int32(),Name.string(),Sex.string(),Age.string(),SibSp.int32(),Parch.int32(),Ticket.string(),Fare.double(),Cabin.string(),Embarked.string()" \
+      --file /data_load/train.csv  --drop
--- service/model/model.go
+++ service/model/model.go
@@ -2,15 +2,15 @@ package model
 
 type Passenger struct {
 	Id                      string  `json:"id,omitempty" bson:"_id,omitempty"`
-	PassengerId             string  `json:"passengerId,omitempty" bson:"PassengerId,omitempty"`
+	PassengerId             int     `json:"passengerId,omitempty" bson:"PassengerId,omitempty"`
 	Survived                bool    `json:"survived,omitempty" bson:"Survived,omitempty"`
 	PassengerClass          int     `json:"passengerClass,omitempty" bson:"PClass,omitempty"`
 	Name                    string  `json:"name,omitempty" bson:"Name,omitempty"`
 	Sex                     string  `json:"sex,omitempty"  bson:"Sex,omitempty"`
-	Age                     int     `json:"age,omitempty"  bson:"Age,truncate"`
+	Age                     string  `json:"age,omitempty"  bson:"Age,truncate"`
 	SiblingsOrSpousesAboard int     `json:"siblingsOrSpousesAboard,omitempty" bson:"SibSp,omitempty"`
 	ParentsOrChildrenAboard int     `json:"parentsOrChildrenAboard,omitempty" bson:"Parch,omitempty"`
-	Ticket                  int     `json:"ticket,omitempty" bson:"Ticket,omitempty"`
+	Ticket                  string  `json:"ticket,omitempty" bson:"Ticket,omitempty"`
 	Fare                    float32 `json:"fare,omitempty" bson:"Fare,truncate"`
 	Cabin                   string  `json:"cabin,omitempty" bson:"Cabin,omitempty"`
 	Embarked                string  `json:"embarked,omitempty" bson:"Embarked,omitempty"`
