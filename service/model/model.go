262d06dedc8221d766bf31484a28c16d1764d9c6
Bryan Freeman <bryanfree66@gmail.com>
2021-09-13 10:49:51 -0500

    Added enum validators to the model. Updated the json to import.

--- service/model/model.go
+++ service/model/model.go
@@ -1,51 +1,85 @@
 package model
 
-type Sex int
+import "errors"
+
+type SexType string
 
 const (
-	Male Sex = iota
-	Female
+	Male   SexType = "male"
+	Female         = "female"
 )
 
-type EmbarkCity int
+func (st SexType) IsValid() error {
+	switch st {
+	case Male, Female:
+		return nil
+	}
+	return errors.New("invalid sex")
+}
+
+type EmbarkType string
 
 const (
-	NoEmbarkedListed EmbarkCity = iota
-	Cherbourg
-	Southampton
-	Queenstown
+	EmbarcationUnknown EmbarkType = "Unknown"
+	Cherbourg                     = "Cherbourg"
+	Southampton                   = "Southampton"
+	Queenstown                    = "Queenstown"
 )
 
-type PassengerClass int
+func (et EmbarkType) IsValid() error {
+	switch et {
+	case EmbarcationUnknown, Cherbourg, Southampton, Queenstown:
+		return nil
+	}
+	return errors.New("invalid embarcation")
+}
+
+type PassengerClassType int
 
 const (
-	NoClassListed PassengerClass = iota
+	ClassNotKnown PassengerClassType = iota
 	First
 	Second
 	Third
 )
 
-type Deck int
+func (pc PassengerClassType) IsValid() error {
+	switch pc {
+	case ClassNotKnown, First, Second, Third:
+		return nil
+	}
+	return errors.New("invalid passenger class")
+}
+
+type DeckType string
 
 const (
-	NoDeckListed Deck = iota
-	A
-	B
-	C
-	D
-	E
-	F
-	G
+	DeckNotKnown DeckType = "Unknown"
+	A                     = "A"
+	B                     = "B"
+	C                     = "C"
+	D                     = "D"
+	E                     = "E"
+	F                     = "F"
+	G                     = "G"
 )
 
+func (dt DeckType) IsValid() error {
+	switch dt {
+	case DeckNotKnown, A, B, C, D, E, F, G:
+		return nil
+	}
+	return errors.New("invalid deck")
+}
+
 type TripData struct {
-	PassengerClass PassengerClass `json:"passengerClass,omitempty" bson:"PassengerClass,omitempty"`
-	Deck           int            `json:"deck,omitempty" bson:"Deck,omitempty"`
-	Cabin          []int          `json:"cabin,omitempty" bson:"Cabin,omitempty"`
-	GroupSize      int            `json:"groupSize,omitempty" bson:"GroupSize,omitempty"`
-	Ticket         string         `json:"ticket,omitempty" bson:"Ticket,omitempty"`
-	Fare           float32        `json:"fare,omitempty" bson:"Fare,truncate"`
-	Embarked       EmbarkCity     `json:"embarked,omitempty" bson:"Embarked,omitempty"`
+	PassengerClass PassengerClassType `json:"passengerClass,omitempty" bson:"PassengerClass,omitempty"`
+	Deck           DeckType           `json:"deck,omitempty" bson:"Deck,omitempty"`
+	Cabin          []int              `json:"cabin,omitempty" bson:"Cabin,omitempty"`
+	GroupSize      int                `json:"groupSize,omitempty" bson:"GroupSize,omitempty"`
+	Ticket         string             `json:"ticket,omitempty" bson:"Ticket,omitempty"`
+	Fare           float32            `json:"fare,omitempty" bson:"Fare,truncate"`
+	Embarked       EmbarkType         `json:"embarked,omitempty" bson:"Embarked,omitempty"`
 }
 
 type Passenger struct {
@@ -56,7 +90,7 @@ type Passenger struct {
 	FirstName   string   `json:"firstName,omitempty" bson:"FirstName,omitempty"`
 	MiddleName  string   `json:"middleNameName,omitempty" bson:"MiddleNameName,omitempty"`
 	LastName    string   `json:"lastName,omitempty" bson:"LastName,omitempty"`
-	Sex         Sex      `json:"sex,omitempty"  bson:"Sex,omitempty"`
+	Sex         SexType  `json:"sex,omitempty"  bson:"Sex,omitempty"`
 	Age         int      `json:"age,omitempty"  bson:"Age,truncate"`
-	TripData    TripData `json:"tripData"`
+	TripData    TripData `json:"tripData"  bson:"TripData"`
 }
